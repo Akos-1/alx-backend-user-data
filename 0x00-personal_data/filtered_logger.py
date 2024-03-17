94a34480605cecc5df4910ab4b358f38ba4a1021
Akos-1 <veronicagyasi@gmail.com>
2024-03-17 13:39:55 -0700

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 
 import logging
-import re
-from typing import List
+from logging import StreamHandler
+from filtered_logger import RedactingFormatter
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "credit_card")
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """Obfuscates log message fields based on specified fields and redaction character."""
-    return re.sub(fr'\b(?:{"|".join(fields)})\b', redaction, message, flags=re.IGNORECASE)
+def get_logger():
+    """Return a logging.Logger object with specified configurations."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    
+    handler = StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    
+    logger.addHandler(handler)
+    
+    return logger
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
-
-    REDACTION = "***"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super().__init__()
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
-        return super().format(record)
+if __name__ == "__main__":
+    # Test the logger
+    logger = get_logger()
+    logger.info
