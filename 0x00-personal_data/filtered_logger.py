be54dc90cac100842b488ecb263a8a58df569550
Akos-1 <veronicagyasi@gmail.com>
2024-03-17 13:32:23 -0700

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
 import logging
+import re
 from typing import List
-from filtered_datum import filter_datum
 
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Obfuscates log message fields based on specified fields and redaction character."""
+    return re.sub(fr'\b(?:{"|".join(fields)})\b', redaction, message, flags=re.IGNORECASE)
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class """
 
     REDACTION = "***"
+    SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        format_str = f"[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-        super().__init__(format_str)
+        super().__init__()
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
