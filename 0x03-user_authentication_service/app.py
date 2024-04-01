a668bc277c38202cbe0f614d6b78830177acd698
Akos-1 <veronicagyasi@gmail.com>
2024-04-01 15:03:44 -0700

    updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -14,6 +16,24 @@ def index() -> str:
     - str: JSON payload.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """
+    Register a new user.
+
+    Returns:
+    - JSON: Response JSON payload.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
 
 
 if __name__ == "__main__":
